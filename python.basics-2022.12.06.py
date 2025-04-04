1b416b76002cfb377efe0fd243f36be645c20042
fa1c0n <114922901+fa1c0nx@users.noreply.github.com>
2025-04-04 09:18:38 +0200

    Update python.basics-2022.12.06.py

--- python.basics-2022.12.06.py
+++ python.basics-2022.12.06.py
@@ -1,32 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import socket
+
 print("This is a vodka-bottle-documentation, sorry, no automation at this time, :-/")
 raise SystemExit
 
 ##########################################################
 
-# prints apple and breaks on banana
+# Prints all fruits and breaks after first iteration
 fruits = ['apple', 'banana', 'cherry']
-for x in fruits:
-    print(x)
-    if fruits == 'banana':
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'banana':
         print()
     break
 
 ##########################################################
 
-# adds the two arrays together and prints
-# according to index number 
+# Combines adjectives with fruits
 adj = ['red', 'big', 'tasty']
 fruits = ['apple', 'banana', 'cherry']
 for x in adj:
     for y in fruits:
-        print(x,y)
+        print(x, y)
 
 ##########################################################
 
-# the following code prints 'found' if number 1 is in the list
-numbers = [1,2,3,4]
+# Check if 1 is in the list
+numbers = [1, 2, 3, 4]
 if 1 in numbers:
     print('found')
 else:
@@ -34,7 +35,7 @@ else:
 
 ##########################################################
 
-# prints all users but removes 'user1'
+# Remove 'user1' if present
 users = ['user1', 'user2', 'user3', 'user4', 'user5']
 if 'user1' in users:
     users.remove('user1')
@@ -42,109 +43,87 @@ if 'user1' in users:
 
 ##########################################################
 
-# prints all users but 'user3'
+# Print all users except 'user3'
 usernames = ['user1', 'user2', 'user3', 'user4', 'user5']
-for x in usernames:
-    if x == 'user3':
+for name in usernames:
+    if name == 'user3':
         continue
-    print(x)
+    print(name)
 
 ##########################################################
 
-# prints 16, 25, 90 and skips the previous
+# Array slicing examples
 array = [1, 4, 9, 16, 25, 90]
-print(array[3:])
-
-# prints 1, 4, 9 and skips the rest
-array = [1, 4, 9, 16, 25, 90]
-print(array[:3])
-
-# prints 9 and 16 but not the rest
-array = [1, 4, 9, 16, 25, 90]
-print(array[2:4])
+print(array[3:])    # 16, 25, 90
+print(array[:3])    # 1, 4, 9
+print(array[2:4])   # 9, 16
 
 ##########################################################
 
-# splits 'Sverige' into 'Sv' 'rig' 'ge'
+# String slicing
 country = 'Sverige'
-print(country[:2])
-print(country[3:6])
-print(country[5:])
+print(country[:2])     # Sv
+print(country[3:6])    # rig
+print(country[5:])     # ge
 
 ##########################################################
 
-# prints min and max number
+# Min, max, sum, and power
 numbers = [1, 2, 3, 4, 5]
-print(min(numbers))
-print(max(numbers))
+print(min(numbers))           # 1
+print(max(numbers))           # 5
 
-# prints sum of numbers
-numbers = [1, 2, 3, 4, 5,6]
-print(sum(numbers))
+numbers = [1, 2, 3, 4, 5, 6]
+print(sum(numbers))           # 21
 
-# prints 2*2*2*2
 x = pow(2, 4)
-print(x)
+print(x)                      # 16
 
 ##########################################################
 
-# prints numbers for 'numbers' and for 'jensen'
-numbers = [1, 2, 3, 4, 5,6]
+# Copying list
+numbers = [1, 2, 3, 4, 5, 6]
 jensen = numbers[:]
 print(numbers)
 print(jensen)
 
 ##########################################################
-# TODO:
-# 1. print the lowest number in array
-# 2. print the highest number in array
-# 3. sum all the elements in the array
-# 4. copy the array and save in variable 'Stockholm'
 
+# TODO: Perform array operations
 num = [1, 5, -29, 86, 7, -8]
 Stockholm = num[:]
-print(min(num))
-print(max(num))
-print(sum(num))
+print(min(num))         # -29
+print(max(num))         # 86
+print(sum(num))         # 62
 print(Stockholm)
 
 ##########################################################
 
-# imports the math module and prints the square root
+# Math operations
 import math
 
 x = math.sqrt(64)
 y = int(math.sqrt(64))
 z = float(math.sqrt(64))
-print(x,y,z)
+print(x, y, z)
 
-# prints the value of pi
-import math
 x = math.pi
-print('the value of pi is', (x))
-print('The value of pi is approximately %f'%(x))
-print('The value of pi is approximately %5.3f' % (x))
+print('The value of pi is', x)
+print('The value of pi is approximately %f' % x)
+print('The value of pi is approximately %5.3f' % x)
 
-# r = radius of circle. prints the square centimeters of the circle
-import math
 r = 4
 area = pow(r, 2) * math.pi
-print('the area of the circle is',(area), 'cm^2')
+print('The area of the circle is', area, 'cm^2')
 
 ##########################################################
-# TODO:
-# - write a script that asks the user to input a number.
-# - if the number is more than 20 then print the square root
-# - of that number.
-# - if not then print the number^2. use a loop
 
-import math
-
-userInput = float(input('please input a number: '))
+# TODO: Number logic with math
+user_input = float(input('Please input a number: '))
 
 while True:
-    if userInput > 20:
-        print(math.sqrt(userInput))
+    if user_input > 20:
+        print(math.sqrt(user_input))
     else:
-        print(math.pow(userInput, 2))
+        print(math.pow(user_input, 2))
     break
