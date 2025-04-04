302677bb4c248389f5170d667c5bb121b3df6c68
fa1c0n <114922901+fa1c0nx@users.noreply.github.com>
2025-04-04 09:19:57 +0200

    Update python.basics-2022.12.09.py

--- python.basics-2022.12.09.py
+++ python.basics-2022.12.09.py
@@ -1,42 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import socket
+
 print("This is a vodka-bottle-documentation, sorry, no automation at this time, :-/")
 raise SystemExit
 
 ##########################################################
-# TODO:
-# - create a function that takes utilizes arrays as parameters.
-# - the function shall add all numbers in the array and then 
-# - add 100 to it. use the following elements:
-# - [100, 300, 400, 3]
+# Add all numbers in array and add 100
 
-def add100(array):
-    sum = 0
-    for elements in array:
-        sum = sum + elements
-    sum = sum + 100
-    return sum
-print(add100([100, 300, 400, 3]))
+def add_100(numbers):
+    return sum(numbers) + 100
+
+print(add_100([100, 300, 400, 3]))
 
 ##########################################################
+# Replace specific characters in string
 
-# TODO:
-# - define a function that takes a string and swaps out
-# - all upper case 'O' to upper case 'Ö' and all
-# - upper case 'A' to lower case 'ä'
-# - replace spaces with dashes
-# - use 'On nAra Ostermalm'.
-
-def characterSwap(string):
-    x = ''
-    for character in string:
-        if character == 'O':
-            x = x + 'Ö'
-        elif character == 'A':
-            x = x + 'ä'
-        elif character == ' ':
-            x = x + '-'
+def character_swap(text):
+    result = ''
+    for char in text:
+        if char == 'O':
+            result += 'Ö'
+        elif char == 'A':
+            result += 'ä'
+        elif char == ' ':
+            result += '-'
         else:
-            x = x + character
-    return x
-print(characterSwap('On nAra Ostermalm'))
+            result += char
+    return result
+
+print(character_swap('On nAra Ostermalm'))
