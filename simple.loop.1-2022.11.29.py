5c5cf5802ea3451d13513fe45181053728957324
fa1c0n <114922901+fa1c0nx@users.noreply.github.com>
2025-04-04 09:21:35 +0200

    Update simple.loop.1-2022.11.29.py

--- simple.loop.1-2022.11.29.py
+++ simple.loop.1-2022.11.29.py
@@ -1,31 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import socket
+
 print("This is a vodka-bottle-documentation, sorry, no automation at this time, :-/")
 raise SystemExit
 
 import os
 
-# main function
 def main():
+    while True:
+        try:
+            weight = float(input("What is your weight? "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
 
-    # loop
-    weight = ''
-while True:
-    weight = float (input('what is your weight? '))
+        if weight < 20:
+            print("Your recommended intake is 500 mg/day")
+        elif 20 <= weight <= 40:
+            print("Your recommended intake is 750 mg/day")
+        elif 41 <= weight <= 60:
+            print("Your recommended intake is 1000 mg/day")
+        else:
+            print("Your recommended intake is 1000 mg/day")
+        break
 
-    if weight < 20:
-        print('Your recommended intake is 500 mg/day')
-        break
-    elif 20 <= weight <= 40:
-        print('Your recommended intake is 750mg/day')
-        break
-    elif 41 <= weight <= 60:
-        print('Your recommended intake is 1000mg/day')
-        break
-    else:
-        print('Your recommended intake is 1000mg/day')
-        break
-        
 if __name__ == '__main__':
-    # invokes main function
     main()
