3e2a7ff3aee340c58c4e9349a7c5676a8b04b571
fa1c0n <114922901+fa1c0nx@users.noreply.github.com>
2025-04-04 09:16:04 +0200

    Update python.basics-2022.11.29.py

--- python.basics-2022.11.29.py
+++ python.basics-2022.11.29.py
@@ -1,101 +1,95 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import socket
+
 print("This is a vodka-bottle-documentation, sorry, no automation at this time, :-/")
 raise SystemExit
 
+# Print numbers from 1 to 16 with step of 3
 for n in range(1, 17, 3):
-	print(n)
+    print(n)
 
-for n in range(1, 5):
-	print('jensen!')
+# Print "jensen!" four times
+for _ in range(4):
+    print("jensen!")
 
+# Infinite loop with nested ranges
 while True:
-	for x in range(1, 12):
-		for y in range(1, 101):
-			for z in range(-100, 6):
-				print (x, y, z)
-				continue
+    for x in range(1, 12):
+        for y in range(1, 101):
+            for z in range(-100, 6):
+                print(x, y, z)
+                continue
 
+# Even numbers from 0 to 100
 for n in range(0, 102, 2):
-	print(n)
+    print(n)
 
+# First 51 even numbers
 for n in range(0, 51):
-	print (2*n)
+    print(2 * n)
 
+# Multiplication table for 1
 number = 1
 for n in range(1, 11):
-   print(number, 'x', n, '=', number*n)
+    print(number, 'x', n, '=', number * n)
 
-for x in range(1,11):
-    print('\n\nTable %d\n' %(x))
-    for y in range(1,11):
-        print('%d x %d = %d' % (x, y, x*y))
+# Full multiplication tables from 1 to 10
+for x in range(1, 11):
+    print(f"\n\nTable {x}\n")
+    for y in range(1, 11):
+        print(f"{x} x {y} = {x * y}")
 
+# Positive number check
 num = 3
 if num > 0:
-	print(num, "is a positive number.")
-	print("This is always printed.")
+    print(num, "is a positive number.")
+    print("This is always printed.")
 
-##########################################################
-# python script
-# - copy and paste all the lines below and paste to
-# - a file and save the file as 'patient.py'
-# - then run the program
-##########################################################
+# ---------------------------------------------------------
+# Save the following scripts in separate files to run
+# ---------------------------------------------------------
 
-#!/usr/bin/env python
+# patient.py
+#!/usr/bin/env python3
+
 import os
 
-# main function
 def main():
+    while True:
+        response = input("Are you an adult? y/n: ").lower()
+        if response == 'y':
+            print("Your recommended intake is 750 mg/day")
+            break
+        elif response == 'n':
+            print("Your recommended intake is 500 mg/day")
+            break
+        else:
+            print("Please answer with 'y' or 'n'")
 
-	# loop
-    specify_input = ''
-while True:
-    specify_input = input('Are you an adult? y/n: ')
-
-    if specify_input.lower() == 'y':
-        print('Your recommended intake is 750 mg/day')
-        break
-    elif specify_input.lower() == 'n':
-        print('Your recommended intake is 500mg/day')
-        break
-    else:
-        print("please answer with a 'y' or 'n' ")
-        continue
-        
 if __name__ == '__main__':
-    # invokes main function
     main()
 
-##########################################################
+# weight_based_dosage.py
+#!/usr/bin/env python3
 
-#!/usr/bin/env python
 import os
 
-# main function
 def main():
+    while True:
+        try:
+            weight = float(input("What is your weight (kg)? "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
 
-	# loop
-    weight = ''
-while True:
-    weight = float (input('what is your weight? '))
+        if weight < 20:
+            print("Your recommended intake is 500 mg/day")
+        elif 20 <= weight <= 40:
+            print("Your recommended intake is 750 mg/day")
+        else:
+            print("Your recommended intake is 1000 mg/day")
+        break
 
-    if weight < 20:
-        print('Your recommended intake is 500 mg/day')
-        break
-    elif 20 <= weight <= 40:
-        print('Your recommended intake is 750mg/day')
-        break
-    elif 41 <= weight <= 60:
-        print('Your recommended intake is 1000mg/day')
-        break
-    else:
-        print('Your recommended intake is 1000mg/day')
-        break
-        
 if __name__ == '__main__':
-    # invokes main function
     main()
-
-##########################################################
