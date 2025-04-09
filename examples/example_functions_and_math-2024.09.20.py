95e383c73f80f62c8209203415ad1100a8a8c420
example <example@example.com>
2025-04-09 13:53:53 +0200

    Translate from swedish to english, fix line 24 wrong translate

--- examples/example_functions_and_math-2024.09.20.py
+++ examples/example_functions_and_math-2024.09.20.py
@@ -21,7 +21,7 @@ def remove_from_list(my_list, element):
 def main():
     my_list = []
     while True:
-        action = input("Do you want to (1) add, (2) write, (3) remove, or (4) exit? ")
+        action = input("Do you want to (1) add, (2) read, (3) remove, or (4) exit? ")
 
         match action:
             case '1':
